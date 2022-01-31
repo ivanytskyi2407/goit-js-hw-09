a4d04d9b391dd061677481fbd221a43253207ba3
ivanytskyi2407 <89008107+ivanytskyi2407@users.noreply.github.com>
2022-01-31 22:28:35 +0200

    1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  endBtn: document.querySelector('button[data-stop]'),
+};
+let timerId = null;
+
+refs.startBtn.addEventListener('click', changeBodyColorOn);
+refs.endBtn.addEventListener('click', changeBodyColorOff);
+
+function changeBodyColorOn() {
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  this.setAttribute('disabled', true);
+  refs.endBtn.removeAttribute('disabled', true);
+}
+function changeBodyColorOff() {
+  clearInterval(timerId);
+  refs.startBtn.removeAttribute('disabled', true);
+  this.setAttribute('disabled', true);
+}
