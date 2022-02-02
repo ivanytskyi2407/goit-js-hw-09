3b896a29f63cebf08b74a63d622a96c88e570bb8
ivanytskyi2407 <ivanytskyi2407@gmail.com>
2022-02-02 19:36:00 +0200

    2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -11,13 +11,13 @@ const refs = {
     dataseconds: document.querySelector('[data-seconds]'),
 }
 refs.startBtn.addEventListener('click', () => { })
+refs.startBtn.disabled = true;
 function convertMs(ms) {
   // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
-
   // Remaining days
   const days = Math.floor(ms / day);
   // Remaining hours
@@ -29,11 +29,11 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
 
-
+// При нажатии на кнопку «Start» начинается отсчет времени до выбранной даты с момента нажатия.
 
 const options = {
   enableTime: true,
@@ -41,10 +41,14 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
     onClose(selectedDates) {
-        if (options.defaultDate != selectedDates) {
-            alert("Please choose a date in the future");
-         }
-      console.log(selectedDates[0]);
+      let currentDate = new Date();
+
+      if (selectedDates[0] <= currentDate) {
+        alert('Please choose a date in the future');
+        return;
+      }
+      refs.startBtn.disabled = false;
+      
   },
 };
 
