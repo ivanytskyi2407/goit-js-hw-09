7ee513a8796aabd17cd42d5173361f5e1f23b5a8
ivanytskyi2407 <89008107+ivanytskyi2407@users.noreply.github.com>
2022-02-02 23:06:15 +0200

    3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,3 +6,11 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+const refs = {
+  delay: document.querySelector('input[name="delay"]'),
+  step: document.querySelector('input[name="step"]'),
+  amout: document.querySelector('input[name="amount"]'),
+  submit: document.querySelector('button[type="submit"]'),
+};
+refs.submit.addEventListener('submit', createPromise);
